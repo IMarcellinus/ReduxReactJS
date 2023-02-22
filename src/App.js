760d031d811669e2d754e25165c6ce315d6346dc
marcel <itsmarsel@gmail.com>
2023-02-22 11:26:25 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import './index.css';
+import logo from './logo.svg'
+import { useSelector,useDispatch } from 'react-redux';
+import { increment, decrement } from './redux/actions/counter';
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='App-header'>
+        <img src={logo} className="App-logo" alt='logo'/>
+        <p>Aplikasi penumlahan dan pengurangan super canggih!</p>
+        <button onClick={() => dispatch(increment())}>tambah 1</button>
+        <button onClick={() => dispatch(decrement())}>kurang 1</button>
+        nilai saat ini: {counter.value}
       </header>
     </div>
+    
   );
 }
 
